0131137927133df4a3a83836c4a46f3ded70eff2
Krzysztof Śmiłek [EXT] <krzysztof.smilek@bitpanda.com>
2022-06-22 09:34:38 +0200

    Extras - actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    id("com.gorylenko.gradle-git-properties") version "2.4.1"
 }
 
 group = "pl.smilasek"
@@ -46,4 +47,16 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+springBoot {
+    buildInfo {
+        properties {
+            version = "someVersion"
+            artifact = rootProject.name
+            additional = mapOf(
+                "extra" to "someValue"
+            )
+        }
+    }
 }
